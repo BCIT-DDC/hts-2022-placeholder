39b8978c63c8d4723028ac08d7935e084fdb711f
matteomiceli <omatteomiceli@gmail.com>
2022-02-07 13:00:13 -0800

    packages added - added prettier, eslint and tailwindcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,8 @@
 module.exports = {
-    siteMetadata: {
-      title: `HtS 2022 Placeholder`,
-        siteUrl: `https://www.yourdomain.tld`,
-    },
-    plugins: [
+  siteMetadata: {
+    title: `HtS 2022 Placeholder`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [`gatsby-plugin-postcss`],
 
-    ]
 }
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+        './public/**/*.html',
+        './src/**/*.{js,jsx,ts,tsx,vue}',
+    ],
+    theme: {
+        fontFamily: {
+
+        },
+        extend: {
+            colors: {
+
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
